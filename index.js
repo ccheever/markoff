fe3007251f7928304b193468664d3e37161cc5a4
Charlie Cheever <ccheever@gmail.com>
2018-08-23 14:58:22 +0100

    default to README.md

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
+let fs = require('fs');
 let path = require('path');
 
 let osascript = require('@expo/osascript');
 
 async function mainAsync(file) {
   if (file) {
+    let filePath = path.resolve(file);
+
+    // If we open a directory, try to open the README.md here
+    if (fs.existsSync(path.join(filePath, 'README.md'))) {
+      filePath = path.join(filePath, 'README.md');
+    }
+
     return await osascript.execAsync(`
 tell application "Markoff"
-    open ${JSON.stringify(path.resolve(file))}
+    open ${JSON.stringify(filePath)}
 end tell
 `);
   } else {
+    // Just open the application if there's no file argument given
     return await osascript.execAsync(`
 tell application "Markoff"
     activate
